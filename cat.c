f25e96b7470aad60ab094109d4bd0f2e5c307e36
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-10-22 10:59:55 +0530

    Update cat.c

--- cat.c
+++ cat.c
@@ -6,9 +6,27 @@
 #include<sys/wait.h>
 
 int main(int argc, char *argv[]){
-    
-    for(int i = 0; i < argc; i++){
-        printf("%s\n", argv[i]);
+    if(strcmp(argv[1], "-E") == 0){
+
+    }
+    else if(strcmp(argv[1], "-n") == 0){
+
+    }
+    else{
+        for(int i = 1; i < argc; i++){
+            FILE* ptr;
+            ptr = fopen(argv[i], "r");
+            if(ptr != NULL){
+                do {
+                ch = fgetc(ptr);
+                printf("%c", ch);
+                } while (ch != EOF);
+                fclose(ptr);
+            }
+            else{
+                printf("cat: %s : No such file or directory\n", argv[i]);
+            }
+        }
     }
 
 }
