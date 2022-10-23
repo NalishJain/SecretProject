73a876b8a73fe8ddb693e8eb6c04b2735f76cec6
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-10-23 20:59:39 +0530

    Update ls.c

--- ls.c
+++ ls.c
@@ -22,4 +22,35 @@ int main(int argc, const char *argv[]){
        } 
        printf("\n");
     }
+    else if(argc == 3){
+
+        if(strcmp(argv[1], "-1") == 0){
+            struct dirent *e1;
+            DIR *directory = opendir(".");
+            while((e1 = readdir(directory)) != NULL){
+                if(e1->d_name[0] == '.'){
+                continue;
+                }
+                printf("%s ", e1->d_name);
+                printf("\n");
+            } 
+        }
+
+        if(strcmp(argv[1], "-a") == 0){
+            struct dirent *e1;
+            DIR *directory = opendir(".");
+            while((e1 = readdir(directory)) != NULL){
+            printf("%s ", e1->d_name);
+            } 
+            printf("\n");            
+        }
+
+        else{
+            printf("Invalid command\n");
+        }
+
+    }
+    else{
+        printf("Invalid command\n")
+    }
 }
