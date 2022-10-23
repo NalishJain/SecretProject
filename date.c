05f830a48ec74094b2cf8996714937e9a6b6086c
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-10-23 22:25:41 +0530

    Update date.c

--- date.c
+++ date.c
@@ -8,22 +8,67 @@
 #include<sys/wait.h>
 #include <dirent.h>
 #include<time.h>
+#include<conio.h>
 
 int main(int argc, char *argv[]){
-    printf("%d\n", argc)
-    if(strcmp(argv[1], "-v") == 0){
+    printf("%d\n", argc);
+    if(strcmp(argv[1], "-u") == 0){
+        if(argc == 3){
+            struct tm *gtime;
+            time_t T;
+
+            T = time(NULL);
+            gtime = gmtime(&T);
+
+            char[7] dayArray = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+            char[12] monArray = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+            printf("%s %s %d %d:%d:%d UTC %d\n", dayArray[gtime->tm_wday], monArray[gtime->tm_mon], gtime->tm_mday, gtime->tm_hour, gtime->tm_min, gtime->tm_sec, gtime->tm_year + 1900);
+
+
+        }
+        
+        else{
+            printf("Invalid command \n");
+        }
 
     }
 
-    else if(strcmp(argv[1], "-v") == 0){
-
+    else if(strcmp(argv[1], "-r") == 0){
+        if(argc == 3){
+                DIR* dir = opendir(argv[2]);
+                if(dir){
+                    struct stat attr;
+                    stat(argv[2], &attr);
+                    printf("%s\n", ctime(&attr.st_mtime));
+                }
+                else{
+                    FILE* fptr = fopen(argv[2], "r");
+                    if(fptr){
+                        struct stat attr;
+                        stat(argv[2], &attr);
+                        printf("%s\n", ctime(&attr.st_mtime));
+                    }
+                    else{
+                        printf("date : no such file or directory exists \n");
+                    }
+                }     
+        }
+        else{
+            printf("Invalid command \n");
+        }
     }
 
     else{
-        
+        if(argc == 2){
         time_t T;
         time(&T);
         printf("%s\n", ctime(&T));
+        }
+
+        else{
+            printf("Invalid command \n");
+        }
 
         
     }
