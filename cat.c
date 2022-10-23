5a7eb2a37cb4b9657ec7ea21eb25fcc32852d570
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-10-23 12:18:01 +0530

    Add files via upload

---
+++ cat.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<limits.h>
+#include<sys/wait.h>
+
+int main(int argc, char *argv[]){
+    if(strcmp(argv[1], "-E") == 0){
+
+        for(int i = 2; i < argc -1; i++){
+            FILE* ptr;
+            ptr = fopen(argv[i], "r");
+            if(ptr != NULL){
+                FILE *fileP; 
+                fileP = fopen("o.txt", "w");
+                char ch;
+                do {
+                ch = fgetc(ptr);
+            
+                if(ch == '\n'){
+                    fputc('$', fileP);
+                    fputc('\n', fileP);
+                }
+                else{
+                    fputc(ch, fileP) ;
+                }
+
+                } while (ch != EOF);
+                fclose(fileP);
+                fclose(ptr);
+
+                ptr = fopen("o.txt", "r");
+                do {
+                ch = fgetc(ptr);
+                printf("%c", ch);
+                } while (ch != EOF);
+                fclose(ptr);
+
+            }
+            else{
+                printf("cat: %s : No such file or directory\n", argv[i]);
+            }
+        }
+    }
+    else if(strcmp(argv[1], "-n") == 0){
+        int counter = 1;
+        for(int i = 2; i < argc -1; i++){
+            FILE* ptr;
+            ptr = fopen(argv[i], "r");
+            if(ptr != NULL){
+                char ch;
+                printf("%d ", counter);
+                do {
+                ch = fgetc(ptr);
+                printf("%c", ch);
+                if(ch == '\n'){
+                    printf("%d ", counter++);
+                }
+                } while (ch != EOF);
+                printf("\n");
+                fclose(ptr);
+            }
+            else{
+                printf("cat: %s : No such file or directory\n", argv[i]);
+            }
+        }
+    }
+    else{
+        
+        for(int i = 1; i < argc -1; i++){
+            FILE* ptr;
+            ptr = fopen(argv[i], "r");
+            if(ptr != NULL){
+                char ch;
+
+                do {
+                ch = fgetc(ptr);
+                printf("%c", ch);
+                } while (ch != EOF);
+                fclose(ptr);
+            }
+            else{
+                printf("cat: %s : No such file or directory\n", argv[i]);
+            }
+        }
+    }
+
+}
