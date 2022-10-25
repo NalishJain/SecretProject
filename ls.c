5247e40dac46504cdb56c6fc2456994ac6cea073
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-10-25 08:58:37 +0530

    Update ls.c

--- ls.c
+++ ls.c
@@ -22,7 +22,6 @@ int main(int argc, const char *argv[]){
        } 
        printf("\n");     
     }
-
     else if(argc == 2){
         if(strcmp(argv[1], "-1") == 0){
             struct dirent *e1;
@@ -45,49 +44,64 @@ int main(int argc, const char *argv[]){
             printf("\n");            
         }
 
-        else if(strlen(argv[1]) != 1){
-            printf("Invalid command \n");
-        }      
-        else{
-       struct dirent *e1;
-       DIR *directory = opendir(".");
-       while((e1 = readdir(directory)) != NULL){
-        if(e1->d_name[0] == '.'){
-            continue;
+        else if(argv[1][0] == '-'){
+            printf("Invalid command\n");
         }
-        printf("%s ", e1->d_name);
-       } 
-       printf("\n");
+
+        else{
+            struct dirent *e1;
+            DIR *directory = opendir(argv[1]);
+            if(directory != NULL){
+            while((e1 = readdir(directory)) != NULL){
+             if(e1->d_name[0] == '.'){
+                continue;
+                }
+            printf("%s ", e1->d_name);
+            } 
+            printf("\n");  }
+            else{
+                printf("Directory does not exist\n");
+            }
         }
     }
-
     else if(argc == 3){
-
         if(strcmp(argv[1], "-1") == 0){
             struct dirent *e1;
-            DIR *directory = opendir(".");
+            
+            DIR *directory = opendir(argv[2]);
+            if(directory != NULL){
             while((e1 = readdir(directory)) != NULL){
                 if(e1->d_name[0] == '.'){
                 continue;
                 }
                 printf("%s ", e1->d_name);
                 printf("\n");
-            } 
+            } }
+            else{
+                printf("Directory does not exist\n");
+            }
         }
 
         else if(strcmp(argv[1], "-a") == 0){
             struct dirent *e1;
-            DIR *directory = opendir(".");
+
+            DIR *directory = opendir(argv[2]);
+            if(directory != NULL){
             while((e1 = readdir(directory)) != NULL){
             printf("%s ", e1->d_name);
             } 
-            printf("\n");            
+            printf("\n"); }
+            else{
+                printf("Directory does not exist\n");
+            }     
         }
 
+        else if(argv[1][0] == '-'){
+            printf("Invalid flag\n");
+        }  
         else{
             printf("Invalid command\n");
-        }
-
+        }    
     }
     else{
         printf("Invalid command\n");
