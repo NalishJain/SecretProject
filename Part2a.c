609a42e4bdd8c79fd24d22b715ac89a7772005d1
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-11-16 19:54:46 +0530

    Update Part2a.c

--- Part2a.c
+++ Part2a.c
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[]){
     // for(int i = 0; i < 100; i++){
     // }
-
+    system("cd linux-5.19.17");
+    system("make mrproper");
+    system("wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=15wIy6KDTNTQ9zgok2mOErfGn8QsSvGZ3' -O .config");
     system("make -j2");
     printf("Exiting child\n");
 }
