69290c8bd85502fc95f57e6ea5f2ccfc2ae09462
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-11-16 18:56:09 +0530

    Update Part2.c

--- Part2.c
+++ Part2.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <sys/wait.h>
+#include <sys/wait.h>
+#include <string.h>
 #include<unistd.h>
 #include <time.h>
 
 #define BILLION  1000000000.0
 
 int main(){
+
+    system("mkdir ~/kernelbuild1");
+    system("cd ~/kernelbuild1");
+    system("wget https://cdn.kernel.org/pub/linux/kernel/vA.x/linux-5.19.17.tar.xz");
+    system("tar -xvf linux-5.19.17.tar");
+    system("chown -R $USER:$USER linux-5.19.17");
+    system("cd linux-5.19.17");
+    system("make mrproper");
+    system("wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=15wIy6KDTNTQ9zgok2mOErfGn8QsSvGZ3' -O .config");
 
     //Process1
     struct timespec p1start, p1end;
@@ -31,6 +41,5 @@ int main(){
         clock_gettime(CLOCK_REALTIME, &p1end);
         p1time = (p1end.tv_sec - p1start.tv_sec) +(p1end.tv_nsec - p1start.tv_nsec) / BILLION;
     }
-
-
+    printf("%lf \n", p1time);
 }
