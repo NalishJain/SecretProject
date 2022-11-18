2228f9b16d74be1693ee1f8bf14b8dfb13690386
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-11-18 15:22:38 +0530

    Add files via upload

---
+++ bashscript2.sh
@@ -0,0 +1,3 @@
+cd /home/kernelbuild2/linux-5.19.17
+wget -P /home/kernelbuild2/linux-5.19.17 --no-check-certificate 'https://docs.google.com/uc?export=download&id=15wIy6KDTNTQ9zgok2mOErfGn8QsSvGZ3' -O .config
+make -j2
