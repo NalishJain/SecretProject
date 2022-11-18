4fa34ad28b4a1dc625ade67bc6095eac5bc37a19
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-11-19 00:10:33 +0530

    Update twodcopy.c

--- twodcopy.c
+++ twodcopy.c
@@ -2,18 +2,16 @@
 #include <linux/syscalls.h>
 
 
-SYSCALL_DEFINE4(kernel_2d_memcpy, float**, src, float**, dest, int, len1, int, len2);
+SYSCALL_DEFINE4(kernel_2d_memcpy, float**, src, float**, dest, int, len1, int, len2)
 {
-    float buffer; 
+    float buffer[len2];
+    for(int i3 = 0; i3 < len1; i3++){
+        if (__copy_from_user(buffer, src[i3], sizeof(float)*(len2)))
+            return -EFAULT;
 
-    for(int i = 0; i < len1; i++){
-        for(int j = 0; j < len2; j++){
-            if (__copy_from_user(buffer, src[i][j], sizeof(float)))
-                return -EFAULT;
-
-            if (__copy_to_user(dest[i][j], buffer, sizeof(float)))
-                return -EFAULT;
-        }
+        if (__copy_to_user(dest[i3], buffer, sizeof(float)*(len2)))
+            return -EFAULT;
+ 
     }
 
     return 0;
