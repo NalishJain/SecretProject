99157eb8ec33d261fa33c613030e02749677013b
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-11-18 15:18:26 +0530

    Delete bashscript.sh

--- bashscript.sh
+++
@@ -1,9 +0,0 @@
-mkdir /home/kernelbuild1
-cd /home/kernelbuild1
-wget -P /home/kernelbuild1/ https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.17.tar.xz
-unxz linux-5.19.17.tar.xz 
-tar -xvf linux-5.19.17.tar
-chown -R $USER:$USER linux-5.19.17
-cd /home/kernelbuild1/linux-5.19.17
-wget -P /home/kernelbuild1/linux-5.19.17 --no-check-certificate 'https://docs.google.com/uc?export=download&id=15wIy6KDTNTQ9zgok2mOErfGn8QsSvGZ3' -O .config
-make -j2
