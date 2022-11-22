4c014af9ef0f4a951cb4278a5e2adc3854fed2f6
NalishJain <103190956+NalishJain@users.noreply.github.com>
2022-11-22 13:36:50 +0530

    Delete bashBuilder.sh

--- bashBuilder.sh
+++
@@ -1,6 +0,0 @@
-mkdir /home/kernelbuild1
-cd /home/kernelbuild1
-wget -P /home/kernelbuild1/ https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.17.tar.xz
-unxz linux-5.19.17.tar.xz 
-tar -xvf linux-5.19.17.tar
-chown -R $USER:$USER linux-5.19.17
